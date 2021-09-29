d591fda11a2f3e122834c860506578d9d77ecc10
Utso17 <tanvirrezautso@gmail.com>
2021-09-30 02:10:53 +0600

    php

---
+++ MID_LAB_TASK_PHP_FORM/TASK_2/C/C.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Name Field </title>
+</head>
+
+<body>
+    <form method="get">
+        <fieldset>
+            <legend>Email</legend>
+            <input type="text" name="email" value="<?php
+            if (isset($_REQUEST['submit'])) {
+               $email = $_REQUEST['email'];
+            if ($email == "") {
+               echo "null value...";
+            } else {
+                echo $email;
+            }
+                }
+                ?>">
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+
+</html>
